9474abd45a492fcb9319b613a2b03d83af0cf926
Stephen Moore <stephen@delfick.com>
2014-06-28 11:01:30 +1000

    Use unicode in python2 at this point (Closes #4)

--- dict2xml/logic.py
+++ dict2xml/logic.py
@@ -116,7 +116,7 @@ class Node(object):
                 children.append(Node("", self.wrap, item))
 
         else:
-            val = str(data)
+            val = six.text_type(data)
             if self.tag:
                 val = "<%s>%s</%s>" % (self.tag, val, self.tag)
 
