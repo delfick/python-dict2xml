aa88f3b69bd0c5b2331ab6b450f2bb9b9f0db5f7
Stephen Moore <stephen@delfick.com>
2024-01-16 18:07:22 +1100

    ci: test with python 3.12

--- tools/bootstrap_venvstarter.py
+++ tools/bootstrap_venvstarter.py
@@ -8,11 +8,13 @@ if not deps_dir.exists():
     deps_dir.mkdir()
 
 if not (deps_dir / "venvstarter.py").exists():
+    if "PIP_REQUIRE_VIRTUALENV" in os.environ:
+        del os.environ["PIP_REQUIRE_VIRTUALENV"]
     os.system(f"{sys.executable} -m pip install venvstarter -t {deps_dir}")
 
 venvstarter_module = runpy.run_path(str(deps_dir / "venvstarter.py"))
 
-wanted_version = "0.11.0"
+wanted_version = "0.12.1"
 
 upgrade = False
 VERSION = venvstarter_module.get("VERSION")
@@ -20,10 +22,10 @@ if VERSION is None:
     upgrade = True
 else:
     Version = venvstarter_module["Version"]
-    if Version(wanted_version) < Version(VERSION):
+    if Version(VERSION) != Version(wanted_version):
         upgrade = True
 
 if upgrade:
-    os.system(f"{sys.executable} -m pip install 'venvstarter>={wanted_version}' -t {deps_dir}")
+    os.system(f"{sys.executable} -m pip install -U 'venvstarter=={wanted_version}' -t {deps_dir}")
 
 manager = runpy.run_path(str(deps_dir / "venvstarter.py"))["manager"]
