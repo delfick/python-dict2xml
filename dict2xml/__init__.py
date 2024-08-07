5788e079b774afed883db1a6594bb37af33443dc
Stephen Moore <stephen@delfick.com>
2024-08-08 09:24:30 +1000

    Ensure the arguments to the entrypoint are used correctly
    Prior to this, arguments for build were passed into Converter instead of
    into build
    
    Fixes #26

--- dict2xml/__init__.py
+++ dict2xml/__init__.py
@@ -3,9 +3,22 @@ from dict2xml.logic import Converter, DataSorter, Node
 from .version import VERSION
 
 
-def dict2xml(data, *args, **kwargs):
+def dict2xml(
+    data,
+    wrap=None,
+    indent="  ",
+    newlines=True,
+    iterables_repeat_wrap=True,
+    closed_tags_for=None,
+    data_sorter=None,
+):
     """Return an XML string of a Python dict object."""
-    return Converter(*args, **kwargs).build(data)
+    return Converter(wrap=wrap, indent=indent, newlines=newlines).build(
+        data,
+        iterables_repeat_wrap=iterables_repeat_wrap,
+        closed_tags_for=closed_tags_for,
+        data_sorter=data_sorter,
+    )
 
 
 __all__ = ["dict2xml", "Converter", "Node", "VERSION", "DataSorter"]
